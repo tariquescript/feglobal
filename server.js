a921c10560821862b7cbfaa86d3b7b1b24c6c038
tariquescript <tariquescript@gmail.com>
2025-11-18 16:51:03 +0530

    server.js update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/api/contact", async (req, res) => {
 });
 
 // Fallback to index.html
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
