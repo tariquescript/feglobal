b4b80c736c34805cd9ea9dd5f3b64379696e2e33
Tarique Shaikh <tariquescript@gmail.com>
2025-12-12 00:53:38 +0530

    book free mastercalss added

--- public/js/programsRender.js
+++ public/js/programsRender.js
@@ -3,17 +3,25 @@ const programsContainer = document.getElementById("programsList");
 function renderPrograms() {
   programsData.forEach(program => {
     programsContainer.innerHTML += `
-      <div class="bg-white  rounded-xl shadow-md overflow-hidden hover:shadow-lg transition p-4">
-        <img src="${program.img}" class="w-full h-40 object-cover rounded-lg" />
+     <div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition p-4 flex flex-col">
+  
+  <img src="${program.img}" class="w-full h-40 object-cover rounded-lg" />
 
-        <h3 class="text-xl font-semibold mt-3">${program.title}</h3>
-        <p class="text-sm text-gray-600 mt-1">${program.desc}</p>
+  <h3 class="text-xl font-semibold mt-3">${program.title}</h3>
+  <p class="text-sm text-gray-600 mt-1 flex-grow">${program.desc}</p>
 
-        <span class="inline-block  mt-2 text-xs px-3 py-1 bg-sky-100 text-sky-600 rounded-full">
-          ${program.category}
-        </span>
-       
-      </div>
+  <span class="mt-3 text-xs px-3 py-1 bg-sky-100 text-sky-600 rounded-full self-start">
+    ${program.category}
+  </span>
+
+  <a href="./programform.html"
+     class="mt-4 w-full text-center bg-[#770325] text-white py-2 rounded-lg hover:bg-gray-900 transition block">
+    Book Free Masterclass
+  </a>
+
+</div>
+
+
     `;
   });
 }
